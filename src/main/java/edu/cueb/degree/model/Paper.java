5d6c675b5a954fe7701a281f8e5a711fe7b56d0c
wangzhao <32155950+Wangzhaozz@users.noreply.github.com>
2017-10-14 23:58:29 +0800

    database

---
+++ src/main/java/edu/cueb/degree/model/Paper.java
@@ -0,0 +1,102 @@
+package edu.cueb.degree.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Paper {
+    @Id
+    @Column(length = 10)
+    private String id;
+    @Column(length = 10)
+    private String firstauthorid;
+    @Column(length = 10)
+    private String papertype;
+    @Column(length = 200)
+    private String papertitle;
+    @Column(length = 10)
+    private String firstauthtype;
+    @Column(length = 10)
+    private String firstauthor;
+    @Column(length = 30)
+    private String belongcollege;
+    @Column(length = 20)
+    private String publishtime;
+    @Column(length = 200 )
+    private String publishjournals;
+    @Column(length = 20)
+    private String journaltype;
+
+    public String getFirstauthorid() {
+        return firstauthorid;
+    }
+
+    public void setFirstauthorid(String firstauthorid) {
+        this.firstauthorid = firstauthorid;
+    }
+
+    public String getPapertype() {
+        return papertype;
+    }
+
+    public void setPapertype(String papertype) {
+        this.papertype = papertype;
+    }
+
+    public String getPalertitle() {
+        return papertitle;
+    }
+
+    public void setPalertitle(String palertitle) {
+        this.papertitle = palertitle;
+    }
+
+    public String getFirstauthtype() {
+        return firstauthtype;
+    }
+
+    public void setFirstauthtype(String firstauthtype) {
+        this.firstauthtype = firstauthtype;
+    }
+
+    public String getFirstauthor() {
+        return firstauthor;
+    }
+
+    public void setFirstauthor(String firstauthor) {
+        this.firstauthor = firstauthor;
+    }
+
+    public String getBelongcollege() {
+        return belongcollege;
+    }
+
+    public void setBelongcollege(String belongcollege) {
+        this.belongcollege = belongcollege;
+    }
+
+    public String getPublishtime() {
+        return publishtime;
+    }
+
+    public void setPublishtime(String publishtime) {
+        this.publishtime = publishtime;
+    }
+
+    public String getPublishjournals() {
+        return publishjournals;
+    }
+
+    public void setPublishjournals(String publishjournals) {
+        this.publishjournals = publishjournals;
+    }
+
+    public String getJournaltype() {
+        return journaltype;
+    }
+
+    public void setJournaltype(String journaltype) {
+        this.journaltype = journaltype;
+    }
+}
---
+++ src/main/java/edu/cueb/degree/model/Project.java
@@ -0,0 +1,130 @@
+package edu.cueb.degree.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Project {
+    @Id
+    @Column(length = 10)
+    private String id;
+    @Column(length = 10)
+    private String principal;
+    @Column(length = 500)
+    private String projectname;
+    @Column(length = 50)
+    private String belongcollege;
+    @Column(length = 200)
+    private String projectmember;
+    @Column(length = 200)
+    private String projectclassification;
+    @Column(length = 20)
+    private String subclassofproject;
+    @Column(length = 10)
+    private String projectlevel;
+    @Column(length = 100)
+    private String projectsourceunit;
+    @Column(length = 20 )
+    private String projecttime;
+    @Column(length = 20)
+    private String plantime;
+    @Column(length = 100)
+    private String disciplineclassification;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPrincipal() {
+        return principal;
+    }
+
+    public void setPrincipal(String principal) {
+        this.principal = principal;
+    }
+
+    public String getProjectname() {
+        return projectname;
+    }
+
+    public void setProjectname(String projectname) {
+        this.projectname = projectname;
+    }
+
+    public String getBelongcollege() {
+        return belongcollege;
+    }
+
+    public void setBelongcollege(String belongcollege) {
+        this.belongcollege = belongcollege;
+    }
+
+    public String getProjectmember() {
+        return projectmember;
+    }
+
+    public void setProjectmember(String projectmember) {
+        this.projectmember = projectmember;
+    }
+
+    public String getProjectclassification() {
+        return projectclassification;
+    }
+
+    public void setProjectclassification(String projectclassification) {
+        this.projectclassification = projectclassification;
+    }
+
+    public String getSubclassofproject() {
+        return subclassofproject;
+    }
+
+    public void setSubclassofproject(String subclassofproject) {
+        this.subclassofproject = subclassofproject;
+    }
+
+    public String getProjectlevel() {
+        return projectlevel;
+    }
+
+    public void setProjectlevel(String projectlevel) {
+        this.projectlevel = projectlevel;
+    }
+
+    public String getProjectsourceunit() {
+        return projectsourceunit;
+    }
+
+    public void setProjectsourceunit(String projectsourceunit) {
+        this.projectsourceunit = projectsourceunit;
+    }
+
+    public String getProjecttime() {
+        return projecttime;
+    }
+
+    public void setProjecttime(String projecttime) {
+        this.projecttime = projecttime;
+    }
+
+    public String getPlantime() {
+        return plantime;
+    }
+
+    public void setPlantime(String plantime) {
+        this.plantime = plantime;
+    }
+
+    public String getDisciplineclassification() {
+        return disciplineclassification;
+    }
+
+    public void setDisciplineclassification(String disciplineclassification) {
+        this.disciplineclassification = disciplineclassification;
+    }
+}
