932de548f6e0ece422bf59893812f5bb09c75c03
jiangxingqi <jxq0816@126.com>
2017-10-20 21:41:45 +0800

    jiangxingqi : 学院查询接口

--- src/main/java/edu/cueb/degree/control/SchoolController.java
+++ src/main/java/edu/cueb/degree/control/SchoolController.java
@@ -14,4 +14,10 @@ public class SchoolController {
 	School add(School school) {
 		return this.schoolRepository.save(school);
 	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/query")
+	School get(School school) {
+		School rs= this.schoolRepository.findById(1).get();
+		return rs;
+	}
 }
