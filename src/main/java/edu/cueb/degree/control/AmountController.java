dff235be931dee6c8e837d48aed080058b56fab2
小樊樊 <t115608602@163.com>
2017-10-21 17:50:41 +0800

    fanxiaojin : 在编人员年龄结构

---
+++ src/main/java/edu/cueb/degree/control/AmountController.java
@@ -0,0 +1,29 @@
+package edu.cueb.degree.control;
+
+import edu.cueb.degree.model.Amount;
+import edu.cueb.degree.model.Structure;
+import edu.cueb.degree.model.StructureRepository;
+import edu.cueb.degree.repository.AmountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.RequestWrapper;
+
+@RestController
+@RequestMapping
+public class AmountController {
+    @Autowired
+    private AmountRepository amountRepository;
+
+    @RequestMapping(method = RequestMethod.GET,value = "/add")
+    Amount add (Amount amount){return this.amountRepository.save(amount);}
+
+    @RequestMapping(method=RequestMethod.GET,value = "/query/{amountId}")
+    Amount get(@PathVariable Integer amountId){
+        return this.amountRepository.findById(amountId).get();
+
+    }
+}
