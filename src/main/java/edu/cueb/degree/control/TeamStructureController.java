ac388d7c8639844a17c5f483fff593eaee8382fe
小樊樊 <t115608602@163.com>
2017-10-21 16:37:03 +0800

    fanxiaojin : 在编人员年龄结构

---
+++ src/main/java/edu/cueb/degree/control/TeamStructureController.java
@@ -0,0 +1,28 @@
+package edu.cueb.degree.control;
+
+import edu.cueb.degree.model.Structure;
+import edu.cueb.degree.model.TeamStructure;
+import edu.cueb.degree.repository.TeamStructureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("structure")
+public class TeamStructureController {
+    @Autowired
+    private TeamStructureRepository teamStructureRepository;
+
+    @RequestMapping(method = RequestMethod.POST, value = "/add")
+    TeamStructure add(TeamStructure teamStructure) {
+        return this.teamStructureRepository.save(teamStructure);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/query/{teamStructureId}")
+    TeamStructure get(@PathVariable Integer teamStructureId) {
+        TeamStructure rs = this.teamStructureRepository.findById(teamStructureId).get();
+        return rs;
+    }
+}
